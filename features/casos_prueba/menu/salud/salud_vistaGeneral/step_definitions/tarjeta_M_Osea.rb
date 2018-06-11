8d51746cc45be18727260499054993c9badc813f
Tu Nombre <alepresta>
2018-06-11 15:41:01 -0300

    Feat: Tarjeta Médula ósea

---
+++ features/casos_prueba/menu/salud/salud_vistaGeneral/step_definitions/tarjeta_M_Osea.rb
@@ -0,0 +1,11 @@
+
+
+Then /^Tarjeta donación de Médula ósea con usuario Donante 20288463213$/ do
+  menu_salir
+  # do something
+end
+
+Then /^Tarjeta donación de Médula ósea con usuario no Donante 27271042669$/ do
+  menu_salir
+  # do something
+end
